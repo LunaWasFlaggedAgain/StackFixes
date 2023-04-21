bcc0a19f332d9f7126990fb5364b0914f77becb6
Allink <arclicious@vivaldi.net>
2023-04-21 07:02:15 +0100

    Do not create busy-waiting "Timer hack thread"

---
+++ src/main/java/com/github/lunawasflaggedagain/stackfixes/mixin/deletions/ClientTimerHackRemover.java
@@ -0,0 +1,12 @@
+package com.github.lunawasflaggedagain.stackfixes.mixin.deletions;
+
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(Minecraft.class)
+public class ClientTimerHackRemover {
+	@Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;initTimerHackThread()V"))
+	public void stackFixes$init$initTimerHackThread(Minecraft instance) {}
+}
