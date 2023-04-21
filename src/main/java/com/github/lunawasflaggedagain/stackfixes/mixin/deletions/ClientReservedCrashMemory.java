51bffe6118430974739f44fd852fdf248832422d
Allink <arclicious@vivaldi.net>
2023-04-21 04:43:21 +0100

    Improve ClientReservedCrashMemory mixin

--- src/main/java/com/github/lunawasflaggedagain/stackfixes/mixin/deletions/ClientReservedCrashMemory.java
+++ src/main/java/com/github/lunawasflaggedagain/stackfixes/mixin/deletions/ClientReservedCrashMemory.java
@@ -1,12 +1,12 @@
 package com.github.lunawasflaggedagain.stackfixes.mixin.deletions;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.util.math.Box;
 import net.minecraft.util.snooper.Snoopable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
@@ -14,12 +14,18 @@ public abstract class ClientReservedCrashMemory implements Snoopable {
 	@Shadow
 	public static byte[] MEMORY_RESERVED_FOR_CRASH = null;
 
-	@Inject(method = "cleanHeap()V", at = @At("HEAD"), cancellable = true)
+	@Redirect(method = "cleanHeap", at = @At(value = "INVOKE", target = "Ljava/lang/System;gc()V"))
+	public void stackFixes$cleanHeap$gc() {
+		//
+	}
+
+	@Inject(method = "cleanHeap", at = @At(value = "FIELD", target = "Lnet/minecraft/client/Minecraft;MEMORY_RESERVED_FOR_CRASH:[B"))
+	public void stackFixes$cleanHeap$PUTSTATIC(CallbackInfo ci) {
+		MEMORY_RESERVED_FOR_CRASH = null;
+	}
+
+	@Inject(method = "cleanHeap()V", at = @At("TAIL"))
 	public void stackFixes$cleanHeap(CallbackInfo ci) {
-		((Minecraft)(Object)this).worldRenderer.deleteGlLists();
-		Box.getCache().clear();
-		((Minecraft)(Object)this).setWorld(null);
 		System.gc();
-		ci.cancel();
 	}
 }
